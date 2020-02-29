9f8bda732f6f755bf6b7af275ec2b63a5b66e1cb
onyeka mmakwe <notmaks@yahoo.com>
2020-02-29 02:34:35 +0100

    initial tailwind setup

---
+++ src/components/TeamList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <TeamMember />
+  </div>
+</template>
+
+<script>
+import TeamMember from "./TeamMember";
+export default {
+  name: "TeamList",
+  components: {
+    TeamMember
+  }
+};
+</script>
---
+++ src/components/TeamMember.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+  >
+    Hi {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Maks'
+    };
+  }
+};
+</script>
