0f5c1cc73699dd25b0a407643d7a1997e9ea10e6
onyeka mmakwe <notmaks@yahoo.com>
2020-03-03 15:16:36 +0100

    add bin icon

--- src/components/TeamMember.vue
+++ src/components/TeamMember.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="max-w-sm rounded overflow-hidden shadow-lg my-8 .w-full m-auto">
-    <div class="font-bold text-xl mb-2">{{ member.name }}</div>
+  <div
+    class="max-w-sm rounded overflow-hidden shadow-lg my-8 .w-full m-auto flex justify-between p-4 "
+  >
+    <h1 class="font-bold text-xl mb-2">{{ member.name }}</h1>
+    <img src="../assets/bin.svg" alt="bin" width="25" />
   </div>
 </template>
 
