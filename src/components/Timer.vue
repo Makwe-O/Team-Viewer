74dd67e32ebed0a2955f5c7764d96fb13f58d61b
onyeka mmakwe <notmaks@yahoo.com>
2020-03-06 10:43:41 +0100

    add margin to buttons

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,57 +1,41 @@
 <template>
   <Fragment>
     <div v-if="!this.setTime">
-      <form
-        class="  rounded px-8 pt-6 pb-8 mb-4"
-        @submit.prevent="handleSubmit"
-      >
+      <form class="rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="handleSubmit">
         <div class="mb-4">
           <label
             class="block text-gray-700 text-sm font-bold mb-2"
             for="username"
-          >
-            Set Time In Minuites
-          </label>
+          >Set Time In Minuites</label>
           <input
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="username"
             type="number"
-            placeholder="1"
+            placeholder="Enter time in muites. e.g 1"
             v-model="time"
           />
           <button
+            @click="startTimer"
             type="submit"
-            class="bg-teal-500 hover:bg-teal-700 text-white  py-2 px-4 rounded mt-4"
-          >
-            Begin
-          </button>
+            class="bg-teal-500 hover:bg-teal-700 text-white py-2 px-4 rounded mt-4"
+          >Begin</button>
         </div>
       </form>
     </div>
     <div v-else>
-      <h1 class="timer">1:00:00</h1>
+      <h1 class="timer">
+        {{ minuite.toLocaleString(undefined, { minimumIntegerDigits: 2 }) }}:{{
+        second.toLocaleString(undefined, { minimumIntegerDigits: 2 })
+        }}
+      </h1>
       <div>
-        <button
-          class="bg-teal-500 hover:bg-teal-700 text-white  py-2 px-4 rounded mx-2"
-        >
-          Start
-        </button>
-        <button
-          class="bg-teal-500 hover:bg-teal-700 text-white  py-2 px-4 rounded  mx-2"
-        >
-          Pause
-        </button>
-        <button
-          class="bg-teal-500 hover:bg-teal-700 text-white  py-2 px-4 rounded  mx-2"
-        >
-          Reset
-        </button>
+        <button class="bg-teal-500 hover:bg-teal-700 text-white py-2 px-4 rounded mx-2 my-1">Start</button>
+        <button class="bg-teal-500 hover:bg-teal-700 text-white py-2 px-4 rounded mx-2 my-1">Pause</button>
+        <button class="bg-teal-500 hover:bg-teal-700 text-white py-2 px-4 rounded mx-2 my-1">Reset</button>
         <button
           @click="handSetNewTime"
-          class="bg-teal-500 hover:bg-teal-700 text-white  py-2 px-4 rounded mx-2"
-        >
-          Set Time
-        </button>
+          class="bg-teal-500 hover:bg-teal-700 text-white py-2 px-4 rounded mx-2"
+        >Set Time</button>
       </div>
     </div>
   </Fragment>
@@ -59,6 +43,7 @@
 
 <script>
 import { Fragment } from "vue-fragment";
+
 export default {
   name: "Timer",
   components: {
@@ -67,15 +52,37 @@ export default {
   data() {
     return {
       setTime: false,
-      time: null
+      time: null,
+      minuite: 0,
+      second: 0
     };
   },
   methods: {
+    setupCountdown() {
+      let secondConverter = Number(this.time) * 60;
+      this.minuite = Math.floor(secondConverter / 60);
+      this.second = secondConverter - this.minuite * 60;
+    },
     handleSubmit() {
-      if (this.time !== null) this.setTime = true;
+      if (Number(this.time) <= 0) {
+        this.$toasted.show(`Please add a number greater than 1`, {
+          theme: "toasted-primary",
+          position: "top-center",
+          duration: 4000
+        });
+      } else {
+        this.setTime = true;
+        this.setupCountdown();
+      }
     },
     handSetNewTime() {
       this.setTime = false;
+    },
+    startTimer() {
+      console.log("start");
+    },
+    pauseTimer() {
+      console.log("pause");
     }
   }
 };
